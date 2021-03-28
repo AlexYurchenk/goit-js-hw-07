199ff1921e4592b3e50e90362e9981f56b7f17c6
AlexYurchenk <75500247+AlexYurchenk@users.noreply.github.com>
2021-03-28 16:53:27 +0300

    Update task-7.js

--- js/task-7.js
+++ js/task-7.js
@@ -1,7 +1,6 @@
 const inputEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
 inputEl.addEventListener('input', event =>{
-    console.log(textEl.style.fontSize)
     textEl.style.fontSize = inputEl.value+`px`
 })
 
