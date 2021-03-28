7ea4f297dabb941ceba0647e814b1e1fd3c3352f
AlexYurchenk <75500247+AlexYurchenk@users.noreply.github.com>
2021-03-28 17:25:09 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -3,11 +3,8 @@ const categoriesItemSearchEl = selectedByIdCategoriesEl.children.length;
 console.log(`В списке ${categoriesItemSearchEl} категории.`);
 const selectedByClassItemEl = document.querySelectorAll('.item');
 
-
-for (const item of selectedByClassItemEl ){
+selectedByClassItemEl.forEach(item =>{
   const titleSelectedEl = item.firstElementChild.textContent;
-   const listSelectedEl = item.lastElementChild.children.length
-   console.log(`Категшория :${titleSelectedEl}
-   Количество элементов: ${listSelectedEl}`);
-}
-
+  const listSelectedEl = item.lastElementChild.children.length;
+  console.log(`Категория: ${titleSelectedEl},Количество элементов: ${listSelectedEl}`)
+})
