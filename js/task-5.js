dfc6e3e3e9b3b385eb9cfe69237390431c5cf423
AlexYurchenk <75500247+AlexYurchenk@users.noreply.github.com>
2021-03-28 17:02:23 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -2,7 +2,7 @@ const inputEl =  document.querySelector('#name-input');
 const spamEl = document.querySelector('#name-output');
 inputEl.addEventListener('input' , (event) => {
   spamEl.textContent = event.currentTarget.value;
-  if (event.currentTarget.value === ``){
+  if (event.currentTarget.value === ''){
     spamEl.textContent = `незнакомец`
   }
 });
